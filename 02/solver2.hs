670b1fa33149fc5c0f4a51657b24acb6e9cbc95a
Csaba Filip <filip.csaba@email.cz>
2024-12-02 20:44:07 +0100

    02b

---
+++ 02/solver2.hs
@@ -0,0 +1,27 @@
+import Text.Regex.TDFA ( (=~), AllMatches(getAllMatches), AllTextMatches (getAllTextMatches) )
+import Prelude hiding (lookup)
+import Data.List.Split (splitOn)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let inputLines = lines contents
+  let reports = map parseReport inputLines
+  let numSafe = length $ filter isSafe reports
+  print numSafe
+
+parseReport :: String -> [Int]
+parseReport str = map read $ splitOn " " str
+
+isSafe :: [Int] -> Bool
+isSafe report = any isSafe' $ report : singleLvlRemovals report
+
+singleLvlRemovals :: [Int] -> [[Int]]
+singleLvlRemovals [] = []
+singleLvlRemovals (x:xs) = xs : map (x :) (singleLvlRemovals xs)
+
+isSafe' :: [Int] -> Bool
+isSafe' report = (allSubs (uncurry (<)) report || allSubs (uncurry (>)) report) && allSubs (\(a, b) -> abs (a-b) >= 1 && abs (a-b) <= 3) report
+
+allSubs :: ((Int, Int) -> Bool) -> [Int] -> Bool
+allSubs cond report = all cond $ zip report (tail report)

b72983253c2ff2b6a1b2ac254d5b2c26783c1fdd
Csaba Filip <filip.csaba@email.cz>
2024-12-03 22:59:15 +0100

    03b

---
+++ 03/solver2.hs
@@ -0,0 +1,27 @@
+import Text.Regex.TDFA ( (=~), AllMatches(getAllMatches), AllTextMatches (getAllTextMatches) )
+import Prelude hiding (lookup)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let inputLine = concat $ lines contents
+  let regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
+  let matches = getAllTextMatches (inputLine =~ regex) :: [String]
+  let res = foldl sumProdIfEnabled (0, True) matches
+  print res
+  -- let numPairsToMul = map matchPair muls
+  -- let result = sum $ map (uncurry (*)) numPairsToMul
+  -- print result
+
+sumProdIfEnabled :: (Int, Bool) -> String -> (Int, Bool)
+sumProdIfEnabled (s, e) str
+  | str == "do()" = (s, True)
+  | str == "don't()" = (s, False)
+  | not e = (s, e)
+  | e = (s + p, e) where
+    (a, b) = matchPair str
+    p = a * b
+
+matchPair :: String -> (Int, Int)
+matchPair str = (read a, read b) where
+  [a, b] = getAllTextMatches (str =~ "[0-9]+") :: [String]

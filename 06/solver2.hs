7c762805b63f9c5b25433089ff152b22b22bde0d
Csaba Filip <filip.csaba@email.cz>
2024-12-07 22:41:37 +0100

    07a and b

--- 06/solver2.hs
+++ 06/solver2.hs
@@ -1,9 +1,10 @@
 import Prelude hiding (lookup)
 import qualified Data.Set as S
 import Data.List (nub)
+import Language.Haskell.TH (doE)
 
 type Pos = (Int, Int)
-data Ori = U | D | L | R deriving Show
+data Ori = U | D | L | R deriving (Eq, Ord, Show)
 
 main :: IO ()
 main = do
@@ -13,6 +14,30 @@ main = do
   let (blocks, guard) = initializeWorld inputLines
   let guardRoute = takeWhile (not . guardOutsideMap mapSize) $ iterate (simulateGuard mapSize blocks) guard
   print $ length $ nub $ map fst guardRoute
+  let loopCauses = filter (checkLoop mapSize blocks) guardRoute
+  print guard
+  print $ head loopCauses
+  print $ length 
+    $ filter (\pos -> pos /= fst guard) 
+    $ nub $ map fst loopCauses
+
+checkLoop :: Int -> S.Set Pos -> (Pos, Ori) -> Bool
+checkLoop mapSize blocks (pos, ori) = checkLoop' mapSize blocks' S.empty (pos, ori) where
+  newBlockPos = nextPos pos ori
+  blocks' = S.insert newBlockPos blocks
+
+checkLoop' :: Int -> S.Set Pos -> S.Set (Pos, Ori) -> (Pos, Ori) -> Bool
+checkLoop' mapSize blocks visited (pos, ori)
+  | guardOnMapEdge mapSize (pos, ori) = False
+  | S.member (pos, ori) visited && S.size visited > 4 = True
+  | otherwise = checkLoop' mapSize blocks visited' (pos', ori') where
+    visited' = S.insert (pos, ori) visited
+    next = nextPos pos ori
+    nextPosBlocked = S.member next blocks
+    (pos', ori') = if nextPosBlocked then (pos, updateOri ori) else (next, ori)
+
+guardOnMapEdge :: Int -> (Pos, Ori) -> Bool
+guardOnMapEdge s ((r, c), _) = r == 0 || r == (s - 1) || c == 0 || c == (s - 1)
 
 simulateGuard :: Int -> S.Set Pos -> (Pos, Ori) -> (Pos, Ori)
 simulateGuard s blocks (pos, ori) = (pos', ori') where
@@ -21,10 +46,10 @@ simulateGuard s blocks (pos, ori) = (pos', ori') where
   (pos', ori') = if nextPosBlocked then (pos, updateOri ori) else (next, ori)
 
 updateOri :: Ori -> Ori
-updateOri U = R 
-updateOri R = D 
-updateOri D = L 
-updateOri L = U 
+updateOri U = R
+updateOri R = D
+updateOri D = L
+updateOri L = U
 
 nextPos :: Pos -> Ori -> Pos
 nextPos (r, c) U = (r-1, c)
---
+++ 07/solver.hs
@@ -0,0 +1,26 @@
+import Text.Regex.TDFA ( (=~), AllMatches(getAllMatches), AllTextMatches (getAllTextMatches) )
+import Prelude hiding (lookup)
+import Data.List.Split (splitOn)
+
+type Equation = (Int, [Int])
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let inputLines = lines contents
+  let equations = map parseEq inputLines
+  let validEquations = filter isValidEq equations
+  print $ sum $ map fst validEquations
+
+isValidEq :: Equation -> Bool
+isValidEq (res, n:ns) = elem res $ generateResults n ns
+
+generateResults :: Int -> [Int] -> [Int]
+generateResults acc [] = [acc]
+generateResults acc (n:ns) = generateResults (acc + n) ns ++ generateResults (acc * n) ns
+
+parseEq :: String -> Equation
+parseEq str = (res, nums) where
+  [resStr, numsStr] = splitOn ": " str
+  res = read resStr
+  nums = map read $ splitOn " " numsStr
